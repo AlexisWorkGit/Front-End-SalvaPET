9439a5de2c70cc1520a315cd2ff94c1b164e3639
AlexisWorkGit <73404237+AlexisWorkGit@users.noreply.github.com>
2022-07-20 13:17:39 -0500

    Solución Logo

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -1,28 +1,37 @@
 import React from "react";
-function Newsletter(){
-    return(
-        <div className="newsletter-area pb-110">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="newsletter-wrap">
-                <div className="newsletter-content">
-                  <h2 className="title">Newsletter For</h2>
-                  <p><span>*</span> Do Not Show Your Email.</p>
-                </div>
-                <div className="newsletter-form">
-                  <form >
-                    <input type="email" placeholder="Enter Your Email..." />
-                    <button type="submit" className="btn">Subscribe</button>
-                  </form>
-                </div>
-                <div className="newsletter-shape"><img src="img/images/newsletter_shape01.png" alt="" /></div>
-                <div className="newsletter-shape shape-two"><img src="img/images/newsletter_shape02.png" alt="" /></div>
+function Newsletter() {
+  return (
+    <div className="newsletter-area pb-110">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="newsletter-wrap">
+              <div className="newsletter-content">
+                <h2 className="title">Suscribete</h2>
+                <p>
+                  <span>*</span> Para recibir más noticias.
+                </p>
+              </div>
+              <div className="newsletter-form">
+                <form>
+                  <input type="email" placeholder="Ingrese su correo..." />
+                  <button type="submit" className="btn">
+                    Enviar
+                  </button>
+                </form>
+              </div>
+              <div className="newsletter-shape">
+                <img src="img/images/newsletter_shape01.png" alt="" />
+              </div>
+              <div className="newsletter-shape shape-two">
+                <img src="img/images/newsletter_shape02.png" alt="" />
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
-export default Newsletter
+export default Newsletter;
+
